cc429dd5a2d68648df597372e95fd29b400dff42
Diyana Urumcheva <d.urumcheva@gmail.com>
2019-01-15 19:42:16 +0000

    Routed pages navigation

--- painmapper/App.js
+++ painmapper/App.js
@@ -1,13 +1,37 @@
+console.disableYellowBox = ["Unable to symbolicate"];
+
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View, Text} from 'react-native';
-import RegisterPage from './application/src/pages/RegisterPage'; 
 
-export default class App extends Component {
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+import LoginPage from './application/src/pages/LoginPage';
+import RegisterPage from './application/src/pages/RegisterPage';
+import ForgotPassPage from './application/src/pages/ForgotPassPage';
+
+class App extends Component {
   render() {
     return (
       <View>
-        <RegisterPage />
+        <AppContainer />
       </View>
     );
   }
 }
+
+const AppStackNavigator = createStackNavigator(
+  {
+    LoginPage: LoginPage, 
+    RegisterPage: RegisterPage, 
+    ForgotPassPage: ForgotPassPage
+  }, 
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: 'steelblue'
+      } 
+    }
+  }
+)
+
+export default AppContainer = createAppContainer(AppStackNavigator);
