32482c5c92a67b7664e4feb550709cd34263c1de
Diyana Urumcheva <d.urumcheva@gmail.com>
2019-01-15 01:15:16 +0000

    Added missing component in RegisterPage: Already have an account? Sign in

--- painmapper/App.js
+++ painmapper/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, View, Text} from 'react-native';
-import ForgotPassPage from './application/src/pages/ForgotPassPage'; 
+import RegisterPage from './application/src/pages/RegisterPage'; 
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <ForgotPassPage />
+        <RegisterPage />
       </View>
     );
   }
--- painmapper/application/src/pages/RegisterPage.js
+++ painmapper/application/src/pages/RegisterPage.js
@@ -1,20 +1,30 @@
-import React, {Component} from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import RegisterForm from '../components/RegisterForm';
 import Logo from '../components/Logo';
 
 export default class LoginPage extends Component {
 
     render() {
-        return(
-            <View style={styles.container}> 
-                    <View> 
-                        <Logo />
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Logo />
                     <Text style={styles.textPrompt}>
-                        Create an account: 
+                        Create an account:
                     </Text>
                     <RegisterForm />
+                    <View style={styles.loginView}>
+                        <Text style={styles.loginText}>
+                            Already have an account?
+                        </Text>
+                        <TouchableOpacity>
+                            <Text style={styles.loginButton}>
+                                Sign in
+                            </Text>
+                        </TouchableOpacity>
                     </View>
+                </View>
             </View>
         );
     }
@@ -22,16 +32,35 @@ export default class LoginPage extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flexGrow: 1, 
-        backgroundColor: 'steelblue', 
-        alignItems: 'center', 
+        flexGrow: 1,
+        backgroundColor: 'steelblue',
+        alignItems: 'center',
         justifyContent: 'center'
-    }, 
+    },
 
     textPrompt: {
-        textAlign:'center',
-        fontSize: 18, 
+        textAlign: 'center',
+        fontSize: 18,
         fontWeight: '300'
-    } 
+    },
+
+
+    loginView: {
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        paddingVertical: 16,
+        flexDirection: 'row'
+    },
+
+    loginText: {
+        color: 'rgba(255,255,255,0.6)',
+        fontSize: 16
+    },
+    loginButton: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: '500',
+        paddingLeft: 5
+    }
 
 })
