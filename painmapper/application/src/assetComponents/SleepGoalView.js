535c812f39cb39ccefcaf0ba3ddcd6b6e4531ea0
Diyana Urumcheva <d.urumcheva@gmail.com>
2019-03-03 10:25:40 +0000

    Added SleepGoalView

--- painmapper/application/src/assetComponents/SleepGoalView.js
+++ painmapper/application/src/assetComponents/SleepGoalView.js
@@ -1,8 +1,66 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, TouchableOpacity, ToastAndroid } from 'react-native';
+import { DatePicker } from 'react-native-wheel-datepicker';
+import firebase from 'react-native-firebase'
+
+var db = firebase.firestore();
 
 export default class SleepGoalView extends Component {
 
+    constructor() {
+        super()
+
+        this.state = {
+            goalWakeTime: new Date(),
+            goalSleepDuration: 8
+        }
+    }
+
+    updateSleepGoals() {
+        let user = firebase.auth().currentUser
+        db
+          .collection("users")
+          .doc(user.uid)
+          .collection("settings")
+          .doc("sleepGoals")
+          .set({
+            goalSleepDuration: this.state.goalSleepDuration,
+            goalWakeTime: this.state.goalWakeTime
+          })
+          .then(() => {
+            console.log("Document successfully written!");
+          })
+          .catch((error) => {
+            console.log("Error writing document: ", error);
+          });
+      }
+    
+      getSleepGoals() {
+        let user = firebase.auth().currentUser
+        db
+          .collection("users")
+          .doc(user.uid)
+          .collection("settings")
+          .doc("sleepGoals")
+          .get()
+          .then(doc => {
+            if (doc.exists) {
+              this.setState({
+                goalSleepDuration: doc.data().goalSleepDuration,
+                goalWakeTime: doc.data().goalWakeTime,
+              })
+            } else {
+              console.log("No such document!");
+            }
+        })
+          .catch(function (error) {
+            console.log("Error getting document:", error);
+          });
+      }
+
+    componentWillMount() {
+        this.getSleepGoals();
+    }
 
     render() {
         return (
@@ -12,9 +70,32 @@ export default class SleepGoalView extends Component {
                 <Text style={styles.wakeUpText}>
                     What time do you need to wake up?
                 </Text>
+                <View style={styles.timePickerContainer}>
+                    <DatePicker
+                        style={{ backgroundColor: 'transparent' }}
+                        height={10}
+                        date={this.state.goalWakeTime}
+                        textColor='white'
+                        mode="time"
+                        onDateChange={ date => this.setState({ goalWakeTime: date })}
+                    />
+                </View>
                 <Text style={styles.durationText}>
-                    How many hours of sleep do you aim to get?
+                    How much sleep do you aim for?
                 </Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => {
+                        this.updateSleepGoals()
+                        ToastAndroid.showWithGravityAndOffset(
+                            'Sleep goals updated!',
+                            ToastAndroid.SHORT,
+                            ToastAndroid.BOTTOM,
+                            0, 280)
+                    }}>
+                    <Text style={styles.buttonText}>
+                        Update
+            </Text>
+                </TouchableOpacity>
             </ImageBackground>
         )
     }
@@ -38,15 +119,42 @@ const styles = StyleSheet.create({
         color: 'white',
         opacity: 0.5,
         fontSize: 20,
-        top: 150,
+        top: 140,
         paddingHorizontal: 40,
+    },
+    timePickerContainer: {
+        position: 'absolute',
+        top: 160,
+        left: 125,
+        width: 100,
+        height: 120,
+        backgroundColor: `rgba(255, 255, 255, 0)`
     },
     durationText: {
         position: 'absolute',
         color: 'white',
         opacity: 0.5,
         fontSize: 20,
-        top: 350,
-        paddingHorizontal: 40,
-    }
+        top: 370,
+        paddingHorizontal: 30,
+    },
+    button: {
+        position: 'absolute',
+        bottom: 10,
+        width: 100,
+        height: 35,
+        backgroundColor: 'white',
+        opacity: 0.5,
+        borderRadius: 25,
+        alignItems: 'center',
+        textAlign: 'center',
+        paddingVertical: 7,
+    },
+    buttonText: {
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#ffffff',
+        textAlign: 'center',
+        textAlignVertical: 'center'
+    },
 })
